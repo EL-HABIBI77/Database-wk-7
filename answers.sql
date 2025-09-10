4471ab434130c5922100eef4f1ea5f26218cb4ac
EL-HABIBI77 <abdulhafidhahmad12@gmail.com>
2025-09-10 10:29:34 +0300

    Normalization Assignment

---
+++ answers.sql
@@ -0,0 +1,48 @@
+-- Create new table in 1NF
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
+
+-- Insert changed data each product in a separate row
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- Create Orders table OrderID & CustomerName)
+CREATE TABLE Orders2 (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Insert unique order-customer records
+INSERT INTO Orders2 (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Create OrderItems table with OrderID, Product & Quantity
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert order-product details
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
